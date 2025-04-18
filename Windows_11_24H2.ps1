cd6339a09a7f30b461f77a504d2e8c170fd925bc
pwnlads <tfernandes@gmail.com>
2025-04-18 14:56:52 +0100

    Update Windows_11_24H2.ps1

--- Windows_11_24H2.ps1
+++ Windows_11_24H2.ps1
@@ -1,19 +1,12 @@
-Write-Host -ForegroundColor Green "Starting OSDCloud ZTI ..."
-start-sleep -Seconds 3
+Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
+Start-Sleep -Seconds 5
 
-Write-Host -ForegroundColor Green "Setting Display Resolution to 1600"
-Set-DisRes 1600
-start-sleep -Seconds 2
+Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Pro -OSLanguage en-gb -OSLicense Retail -ZTI
 
-Write-Host -ForegroundColor Green "Updating OSD Powershell Module"
-Install-Module OSD -Force
+#Restart from WinPE
 
-Write-Host -ForegroundColor Green "Importing OSD Powershell Module"
-Import-Module OSD -Force
+Write-Host -ForegroundColor Green “Restarting in 20 seconds!”
 
-Write-Host -ForegroundColor Green "Start OSD Cloud"
-Start-OSDCloud -OSLanguage en-gb -OSName 'Windows 11 24H2 x64' -OSLicense Retail -OSEdition Pro -Zti
+Start-Sleep -Seconds 20
 
-Write-Host -ForegroundColor Green "Restarting ..."
-start-sleep -Seconds 5
 wpeutil reboot
